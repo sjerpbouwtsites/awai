35a637221d18884fa42cb59b232583d6724998c9
Sjerp van Wouden <ik@sjerpvanwouden.nl>
2023-11-29 17:21:41 +0100

    /

--- rest.php
+++ rest.php
@@ -90,6 +90,7 @@ function awai_monday_challenge(WP_REST_Request $req)
         // $page->evaluate($remove_image);
 
         // $body_html = $page->evaluate("document.body.innerHTML")->getReturnValue();
+        $body_html = 'nog neits';
 
         $html_file2 = fopen(__DIR__."/post-html2.html", "w") or die("Unable to open file!");
         $html2 = "
